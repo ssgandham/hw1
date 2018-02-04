56411e00ac5d45707c28eca38fdb798557fca9e0
vijayakarthikadirvel <vijayakarthik.kadirvel@gmail.com>
2018-02-04 12:25:02 -0800

    Final COde

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -7,7 +7,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <omp.h>
 #include "sort.hh"
 
 #include <string.h>
@@ -114,8 +114,11 @@ void pmerge_sort(keytype* A, int start, int end, keytype* B, int index) {
 
 void parallelSort(int N, keytype* A) {
 	keytype* B = new keytype[N];
+	#pragma omp parallel
+	#pragma omp single nowait
+	{
 	pmerge_sort(A, 0, N - 1, B, 10);
-  free(B);
+ 	}
 
 }
 /* eof */
