f92f577febc7316367409ce7839eb6be8cf482cf
ssgandham <gandhamsnehalsrinivas@gmail.com>
2018-02-04 11:22:37 -0800

    optimized the code

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -12,86 +12,22 @@
 
 #include <string.h>
 
-
-int number_swap(int *num1, int *num2){
-  int tmp  = *num1;
-      *num1 = *num2;
-      *num2 = tmp;
+int number_swap(int *num1, int *num2) {
+	int tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
 }
-
-static int compare (const void* a, const void* b){
-        keytype ka = *(const keytype *)a;
-        keytype kb = *(const keytype *)b;
-        if (ka < kb)
-                return -1;
-        else if (ka == kb)
-                return 0;
-        else
-                return 1;
-}
-
-void serialMergeForParallel(keytype* T, int start1, int end1, int start2, int end2, keytype* A, int start3) {
-
-        // T[start1] is the first element of the first sub-array
-        // T[end1] is the last element of the first sub-array
-        // T[start2] is the first element of the second sub-array
-        // T[end2] is the last element of the second sub-array
-
-        // A[start3] is the first element where the right value should be written to
-
-        // control variables:
-        int i = start1;
-        int j = start2;
-        int k = start3;
-
-        while ( (i <= end1) && (j <= end2) ) {
-
-                if ( T[i] <= T[j] ) {
-
-                        A[k] = T[i];
-                        i++;
-
-                } else {
-
-                        A[k] = T[j];
-                        j++;
-
-                }
-
-                k++;
-
-        }
-
-        // either array might have elements left
-        // if the first array has elements left:
-        while (i <= end1) {
-
-                A[k] = T[i];
-                i++;
-                k++;
-
-        }
-        // if the second array has elements left:
-        while (j <= end2) {
-
-                A[k] = T[j];
-                j++;
-                k++;
-
-        }
-
-}
-
-
 
 int get_max_number(int numbA, int numbB) {
-	if(numbA>numbB) return numbA;
-  else return numbB;
+	if (numbA > numbB)
+		return numbA;
+	else
+		return numbB;
 }
 
-int binary_search(int low1, int high1, keytype key, keytype* A ) {
-	
-	int low = low1, high = get_max_number(low1, high1+ 1);
+int binary_search(int low1, int high1, keytype key, keytype* A) {
+
+	int low = low1, high = get_max_number(low1, high1 + 1);
 	while (low < high) {
 		int mid = (low + high) / 2;
 		if (key <= A[mid]) {
@@ -103,64 +39,83 @@ int binary_search(int low1, int high1, keytype key, keytype* A ) {
 	return high;
 }
 
-void pmerge(int start1,int start2, int start3, int end1, int end2, int index, keytype* T, keytype* A){
-	int n1 = end1 - start1 + 1;
-	int n2 = end2 - start2 + 1;
-	int Total_ele = n1+n2;
-	if(Total_ele<=index){
-		serialMergeForParallel(T, start1, end1, start2, end2, A, start3);
-	}else{         
-		if (n1 < n2) {
-     			 number_swap(&start1,&start2);
-      			 number_swap(&end1,&end2);
-      			 number_swap(&n1,&n2);
+void pmerge(int start1, int start2, int start3, int end1, int end2, int index,
+		keytype* T, keytype* A) {
+	int len1 = end1 - start1 + 1;
+	int len2 = end2 - start2 + 1;
+
+	if ((len1+len2) <= index) {
+
+		int tmp_start1 = start1, tmp_start2 = start2, tmp_end1 = end1,
+				tmp_end2 = end2, tmp_index = start3;
+
+		while ((tmp_start1 <= tmp_end1) && (tmp_start2 <= tmp_end2)) {
+
+			if (T[tmp_start1] <= T[tmp_start2])
+				A[tmp_index++] = T[tmp_start1++];
+
+			else
+				A[tmp_index++] = T[tmp_start2++];
+
 		}
 
-		if (n1 == 0) {
+		while (tmp_start1 <= tmp_end1)
+			A[tmp_index++] = T[tmp_start1++];
+		while (tmp_start2 <= tmp_end2)
+			A[tmp_index++] = T[tmp_start2++];
+
+	} else {
+		if (len1 < len2) {
+			number_swap(&start1, &start2);
+			number_swap(&end1, &end2);
+			number_swap(&len1, &len2);
+		}
+
+		if (len1 == 0) {
 
 			return;
 
 		} else {
 
 			int mid1 = (start1 + end1) / 2;
-			int mid2 = binary_search(start2, end2,T[mid1], T);
+			int mid2 = binary_search(start2, end2, T[mid1], T);
 			int mid3 = start3 + (mid1 - start1) + (mid2 - start2);
 			A[mid3] = T[mid1];
 
-			#pragma omp task
-      			{
+#pragma omp task
+			{
+				pmerge(start1, start2, start3, mid1 - 1, mid2 - 1, index, T, A);
+			}
 
-	 		pmerge(start1,start2, start3, mid1-1,mid2-1, index, T, A);
-      			}
-
-      			pmerge(mid1+1,mid2,mid3+1,end1,end2,index, T,A);
-      			#pragma omp taskwait
+			pmerge(mid1 + 1, mid2, mid3 + 1, end1, end2, index, T, A);
+#pragma omp taskwait
 		}
 	}
 
 }
 
 void pmerge_sort(keytype* A, int start, int end, keytype* B, int index) {
-    	int n = (end-start)+1;
-	if (n>1) {
-	    int mid = (start+end)/2;
+	int n = (end - start) + 1;
+	if (n > 1) {
+		int mid = (start + end) / 2;
 
-	#pragma omp task
-    	{
-	    pmerge_sort(A, start, mid, B, index); 
-    
-    	    pmerge_sort(A, mid + 1, end, B, index);
-        }
-        #pragma omp taskwait 
-    	pmerge(start, mid + 1, start, mid, end, index, A, B);
-	for(int i=start;i<=end;i++) A[i] = B[i];
+#pragma omp task
+		{
+			pmerge_sort(A, start, mid, B, index);
+    }
+		pmerge_sort(A, mid + 1, end, B, index);
+
+#pragma omp taskwait
+		pmerge(start, mid + 1, start, mid, end, index, A, B);
+		for (int i = start; i <= end; i++)
+			A[i] = B[i];
 	}
 }
 
-void parallelSort (int N, keytype* A) 
-{
-	keytype* B = new keytype[N]; 
-	pmerge_sort(A, 0, N-1, B, 10);
-      
+void parallelSort(int N, keytype* A) {
+	keytype* B = new keytype[N];
+	pmerge_sort(A, 0, N - 1, B, 10);
+  free(B);
+
 }
 /* eof */
